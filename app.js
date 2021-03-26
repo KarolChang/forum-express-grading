d88ecb5b2324e8e5b15d3fe0c06a5b200eefb5c9
Karol <super000999888@gmail.com>
2021-03-26 12:28:52 +0800

    add express-session and flash message for signup page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const db = require('./models')
 const app = express()
@@ -8,7 +10,14 @@ const port = 3000
 
 app.engine('hbs', exphbs({ extname: '.hbs'}))
 app.set('view engine', 'hbs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'MySecret', resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,14 +7,30 @@ const userController = {
     return res.render('signup')
   },
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
-      .then(user => {
-        return res.redirect('/signin')
-      })
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) {
+      req.flash('error_msg', '密碼與確認密碼不相符!')
+      return res.redirect('/signup')
+      // return res.render('signup', { name, email, password, passwordCheck })
+    } else {
+      User.findOne({ where: { email } })
+        .then(user => {
+          if (user) {
+            req.flash('error_msg', '這個信箱已經被註冊過!')
+            return res.redirect('/signup')
+          } else {
+            User.create({
+              name,
+              email,
+              password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+            })
+              .then(() => {
+                req.flash('success_msg', '註冊成功!')
+                return res.redirect('/signin')
+              })
+          }
+        })
+    }
   }
 }
 
