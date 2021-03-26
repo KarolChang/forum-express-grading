40e6dc9f1f91bf75eefd671b8e0bb02a9eebfedd
Karol <super000999888@gmail.com>
2021-03-26 15:39:55 +0800

    add authentication before going to index page

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,21 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
+  // 身分驗證
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) return next()
+    return res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated() && req.user.isAdmin) return next()
+    return res.redirect('/')
+  }
   // 前台
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurants)
+  app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticated, restController.getRestaurants)
   // 後台
-  app.get('/', (req, res) => res.redirect('/admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
   // 註冊
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
